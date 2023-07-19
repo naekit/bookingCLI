39f17392052ed722ca090eef569b5635f3dd3b00
naekit <mykyta.onishchuck@code.berlin>
2023-07-19 16:49:40 -0400

    create the models

--- src/models/Lesson.java
+++ src/models/Lesson.java
@@ -1,4 +1,55 @@
 package models;
 
+import java.util.Objects;
+
 public class Lesson {
+    private String title;
+    private String subject;
+    private Mentor mentor;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Mentor getMentor() {
+        return mentor;
+    }
+
+    public void setMentor(Mentor mentor) {
+        this.mentor = mentor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lesson lesson = (Lesson) o;
+        return Objects.equals(title, lesson.title) && Objects.equals(subject, lesson.subject) && Objects.equals(mentor, lesson.mentor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, subject, mentor);
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" +
+                "title='" + title + '\'' +
+                ", subject='" + subject + '\'' +
+                ", mentor=" + mentor +
+                '}';
+    }
 }
--- src/models/Mentor.java
+++ src/models/Mentor.java
@@ -1,4 +1,35 @@
 package models;
 
+import java.util.Objects;
+
 public class Mentor {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mentor mentor = (Mentor) o;
+        return Objects.equals(name, mentor.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Mentor{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
